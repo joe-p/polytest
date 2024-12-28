edfc4e7c3be4f9d6650e81ca65f228f1f5916dbc
Joe Polny <joepolny@gmail.com>
2024-12-28 09:03:42 -0500

    use jinja for markdown

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct Config {
     #[serde(rename = "suite")]
-    pub suites: Vec<Suite>,
+    pub suites: Vec<SuiteConfig>,
 
     #[serde(rename = "group")]
-    pub groups: Vec<Group>,
+    pub groups: Vec<GroupConfig>,
 
     #[serde(rename = "test")]
-    pub tests: Vec<Test>,
+    pub tests: Vec<TestConfig>,
 }
 
 impl Config {
@@ -19,106 +19,134 @@ impl Config {
     }
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct Suite {
+#[derive(Deserialize, Debug, Clone)]
+pub struct SuiteConfig {
     pub id: String,
     pub name: Option<String>,
     pub groups: Vec<String>,
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct Group {
+#[derive(Deserialize, Debug, Clone)]
+pub struct GroupConfig {
     pub id: String,
     pub tests: Vec<String>,
     pub name: Option<String>,
     pub desc: Option<String>,
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct Test {
+#[derive(Deserialize, Debug, Clone)]
+pub struct TestConfig {
     pub id: String,
     pub name: Option<String>,
     pub desc: Option<String>,
 }
 
+#[derive(Debug, Serialize)]
+pub struct Suite {
+    pub name: String,
+    pub groups: Vec<Group>,
+}
+
+impl Suite {
+    pub fn from_config(config: &Config, suite_config: &SuiteConfig) -> Self {
+        Self {
+            name: suite_config.name.clone().unwrap_or(suite_config.id.clone()),
+            groups: config
+                .groups
+                .iter()
+                .filter(|g| suite_config.groups.contains(&g.id))
+                .map(|g| Group::from_config(config, g))
+                .collect(),
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct Group {
+    pub name: String,
+    pub tests: Vec<Test>,
+    pub desc: String,
+}
+
+impl Group {
+    pub fn from_id(config: &Config, group_id: &str) -> Self {
+        let group_config = config
+            .groups
+            .iter()
+            .find(|g| g.id == group_id)
+            .expect("group should exist");
+        Self::from_config(config, group_config)
+    }
+
+    fn from_config(config: &Config, group_config: &GroupConfig) -> Self {
+        let tests: Vec<Test> = group_config
+            .tests
+            .iter()
+            .map(|test_id| {
+                let test = config
+                    .tests
+                    .iter()
+                    .find(|t| &t.id == test_id)
+                    .expect("test should exist");
+
+                Test {
+                    name: test.name.clone().unwrap_or(test.id.clone()),
+                    desc: test.desc.clone().unwrap_or("".to_string()),
+                }
+            })
+            .collect();
+
+        Self {
+            name: group_config.name.clone().unwrap_or(group_config.id.clone()),
+            tests,
+            desc: group_config.desc.clone().unwrap_or("".to_string()),
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct Test {
+    pub name: String,
+    pub desc: String,
+}
+
+impl Test {
+    pub fn from_config(test_config: &TestConfig) -> Self {
+        Self {
+            name: test_config.name.clone().unwrap_or(test_config.id.clone()),
+            desc: test_config.desc.clone().unwrap_or("".to_string()),
+        }
+    }
+}
+
 fn main() {
     let config = Config::from_file("examples/vehicles/polytest.toml");
-    println!("{:?}", config);
-    generate_markdown(&config);
-    let jinja_value = minijinja::Value::from_serialize(&config);
+
+    let suite_values: Vec<Suite> = config
+        .suites
+        .iter()
+        .map(|s| Suite::from_config(&config, s))
+        .collect();
+
+    let group_values: Vec<Group> = config
+        .groups
+        .iter()
+        .map(|g| Group::from_config(&config, g))
+        .collect();
+
+    let test_values: Vec<Test> = config.tests.iter().map(Test::from_config).collect();
+
     let mut env = minijinja::Environment::new();
     env.add_template("markdown", include_str!("../templates/markdown.jinja"))
         .unwrap();
     let template = env.get_template("markdown").unwrap();
-    println!(
-        "{}",
-        template
-            .render(minijinja::context! { config => jinja_value })
-            .unwrap()
-    );
-}
-
-fn get_anchor(id: &str) -> String {
-    id.replace(" ", "-")
-}
-
-fn generate_markdown(config: &Config) {
-    let mut markdown = String::new();
-    markdown.push_str("# Polytest Test Plan\n");
-
-    markdown.push_str("## Test Suites\n");
-    for suite in &config.suites {
-        markdown.push_str(&format!("\n### {}\n\n", suite.id));
-        markdown.push_str("| Name | Description |\n");
-        markdown.push_str("| --- | --- |\n");
-        for group_id in &suite.groups {
-            let group = config
-                .groups
-                .iter()
-                .find(|g| g.id == *group_id)
-                .expect("group should exist");
-
-            let name = group.name.as_deref().unwrap_or(&group.id);
-
-            markdown.push_str(&format!(
-                "| [{}](#{}) | {} |\n",
-                name,
-                get_anchor(name),
-                group.desc.as_deref().unwrap_or("")
-            ));
-        }
-    }
-
-    markdown.push_str("\n## Test Groups\n");
-    for group in &config.groups {
-        markdown.push_str(&format!("\n### {}\n\n", group.id));
-        markdown.push_str("| Name | Description |\n");
-        markdown.push_str("| --- | --- |\n");
-        for test_id in &group.tests {
-            let test = config
-                .tests
-                .iter()
-                .find(|t| t.id == *test_id)
-                .unwrap_or_else(|| panic!("test {} should exist", test_id));
-
-            let name = test.name.as_deref().unwrap_or(&test.id);
-            markdown.push_str(&format!(
-                "| [{}](#{}) | {} |\n",
-                name,
-                get_anchor(name),
-                test.desc.as_deref().unwrap_or("")
-            ));
-        }
-    }
-
-    markdown.push_str("\n## Test Cases\n");
-    for test in &config.tests {
-        let name = test.name.as_deref().unwrap_or(&test.id);
-        markdown.push_str(&format!("\n### {}\n\n", name));
-        if let Some(desc) = &test.desc {
-            markdown.push_str(&format!("{}\n", desc));
-        }
-    }
+    let markdown = template
+        .render(minijinja::context! {
+            suites => minijinja::Value::from_serialize(&suite_values),
+            groups => minijinja::Value::from_serialize(&group_values),
+            tests => minijinja::Value::from_serialize(&test_values)
+        })
+        .unwrap();
 
     std::fs::write("examples/vehicles/generated_markdown.md", markdown).unwrap();
 }
