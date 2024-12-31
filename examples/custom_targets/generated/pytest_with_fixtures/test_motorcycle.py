764070575778d7c8857d665b9d08ccb2521396dd
Joe Polny <joepolny@gmail.com>
2024-12-31 11:22:10 -0500

    custom test target

---
+++ examples/custom_targets/generated/pytest_with_fixtures/test_motorcycle.py
@@ -0,0 +1,39 @@
+import pytest
+from fixtures import ValidVehicle, InvalidVehicle, BrokenVehicle
+# Polytest Suite: motorcycle
+
+# Polytest Group: broken vehicle
+
+@pytest.mark.group_broken_vehicle
+def test_flat_tire_is_caught(broken_vehicle: BrokenVehicle):
+    """The function for checking tires should return false if the vehicle has a flat tire"""
+    raise Exception("TEST NOT IMPLEMENTED")
+
+@pytest.mark.group_broken_vehicle
+def test_broken_headlight_is_caught(broken_vehicle: BrokenVehicle):
+    """The function for checking headlights should return false if the vehicle has a broken headlight"""
+    raise Exception("TEST NOT IMPLEMENTED")
+
+# Polytest Group: invalid vehicle
+
+@pytest.mark.group_invalid_vehicle
+def test_extra_headlight_throws_error(invalid_vehicle: InvalidVehicle):
+    """Having an extra headlight is invalid and should throw an error"""
+    raise Exception("TEST NOT IMPLEMENTED")
+
+@pytest.mark.group_invalid_vehicle
+def test_extra_tire_throws_error(invalid_vehicle: InvalidVehicle):
+    """Having an extra tire is invalid and should throw an error"""
+    raise Exception("TEST NOT IMPLEMENTED")
+
+# Polytest Group: valid vehicle
+
+@pytest.mark.group_valid_vehicle
+def test_check_tires(valid_vehicle: ValidVehicle):
+    """The function for checking tires should return true if the vehicle has the correct number of inflated tires"""
+    raise Exception("TEST NOT IMPLEMENTED")
+
+@pytest.mark.group_valid_vehicle
+def test_check_headlights(valid_vehicle: ValidVehicle):
+    """The function for checking headlights should return true if the vehicle has the correct number of headlights"""
+    raise Exception("TEST NOT IMPLEMENTED")
