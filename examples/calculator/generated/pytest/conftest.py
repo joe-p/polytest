683a2229d6063514e8078e529d2eab9f8a7cdd81
Joe Polny <joepolny@gmail.com>
2025-02-16 07:06:33 -0500

    remove other examples
    It will be easier to maintain just one example so I've removed other
    examples

--- examples/calculator/generated/pytest/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-
-def pytest_configure(config):
-    config.addinivalue_line("markers", "group_add: Addition tests")
-    config.addinivalue_line("markers", "group_multiply: Multiplication tests")
