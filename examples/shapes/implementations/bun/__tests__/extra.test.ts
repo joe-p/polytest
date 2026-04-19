45f9db3d422065deefdb87f3cb225ee4ab5423cf
Joe Polny <joepolny@gmail.com>
2026-04-19 12:51:27 -0400

    feat: print test desc in ts templates

---
+++ examples/shapes/implementations/bun/__tests__/extra.test.ts
@@ -0,0 +1,18 @@
+import { expect, test, describe } from "bun:test";
+
+describe("extra", () => {
+  // Polytest Suite: extra
+
+  describe("extra", () => {
+    // Polytest Group: extra
+    /* 
+      Here
+      Are
+      New
+      Lines
+     */
+    test("desc_new_line", () => { });
+
+  });
+
+});
---
+++ examples/shapes/implementations/swift/Tests/ShapesTests/ExtraTests.swift
@@ -0,0 +1,13 @@
+import Testing
+
+@testable import Shapes
+
+// Polytest Suite: extra
+
+// Polytest Group: extra
+
+@Test("extra: desc_new_line")
+func extraDescNewLine() throws {
+    assert(true)
+}
+
